df79ec311ac6b840149eebefbb471d1fc4dc6519
Erick Romero <erickromeronieto018@gmail.com>
2023-11-06 21:02:52 -0500

    Se agrego el tablero y el pie de pagina

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
+// import {AngularFireModule} from '@angular/fire';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
