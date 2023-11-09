86345c4e8b95af7bb42826da2ffc2cf6648c4bd8
Erick Romero <erickromeronieto018@gmail.com>
2023-11-09 17:34:58 -0500

    Subiendo la nueva carpeta de services que quedo por fuera del commmit

---
+++ src/app/services/clientes.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { Cliente } from '../interfaces/cliente';
+import {Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteServices {
+  clientesColeccion: AngularFirestoreCollection<Cliente>;
+  clienteDoc!: AngularFirestoreDocument<Cliente>;
+  clientes!: Observable<Cliente[]>;
+  cliente!: Observable<Cliente>;
+  
+  constructor(private firebaseDb: AngularFirestore) {
+    //Se hace la petición a la firebase para que retorne los nombres en orden ascendente 
+    this.clientesColeccion = firebaseDb.collection('clientes', ref => ref.orderBy('nombre', 'asc'))
+  }
+
+  /**---Nos retorna la información de los clientes-----*/
+  getClientes(): Observable<Cliente[]>{
+      //Obtenemos los clientes
+      this.clientes = this.clientesColeccion.snapshotChanges().pipe(
+        map( cambios => {
+          return cambios.map( accion => {
+            const datos = accion.payload.doc.data() as Cliente;
+            datos.id = accion.payload.doc.id;
+            return datos
+          })
+        })
+      )
+      return this.clientes
+  }   
+}
