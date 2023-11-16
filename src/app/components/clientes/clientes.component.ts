3758ac3dbbddead9665ad56c6619892584cb3bf0
Erick Romero <erickromeronieto018@gmail.com>
2023-11-16 14:00:51 -0500

    Se agrego el boton de agregar cliente y la tabla para visualizar la informacion de los clientes

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { Cliente } from 'src/app/interfaces/cliente';
 import { ClienteServices } from 'src/app/services/clientes.service';
 
@@ -8,17 +11,28 @@ import { ClienteServices } from 'src/app/services/clientes.service';
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit{
+  dataSource!: MatTableDataSource<Cliente>;
+  displayedColumns: string[] = ['apellido', 'email', 'id', 'nombre', 'saldo'];
 
   //Variables
   clientes!: Cliente[];
+  booleanCheck: boolean = false;
+
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private clientesService: ClienteServices) {
-
   }
 
   ngOnInit(){
       this.clientesService.getClientes().subscribe(clientesDb => {
           this.clientes = clientesDb;
+
+          //Asignamos la data al datasource
+          this.dataSource = new MatTableDataSource(this.clientes)
+          console.log(this.dataSource.data, this.clientes);
+          this.booleanCheck = true;  
       })
   }
 
