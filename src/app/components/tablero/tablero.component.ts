9af172b8d06f10431e19401aca7cfdd584c7b979
Erick Romero <erickromeronieto018@gmail.com>
2023-11-23 16:56:21 -0500

    Agregué detalles de el subheader y centré la tabla

--- src/app/components/tablero/tablero.component.ts
+++ src/app/components/tablero/tablero.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { OnInit} from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
 @Component({
   selector: 'app-tablero',
   templateUrl: './tablero.component.html',
   styleUrls: ['./tablero.component.css']
 })
-export class TableroComponent {
+export class TableroComponent implements OnInit{
+
+  isLoggedIn: boolean = false;
+
+  constructor(private loginService: LoginService){}
+
+  ngOnInit(){
+    this.loginService.getAuth().subscribe( auth => {
+      if(auth){
+        this.isLoggedIn = true;
+      }
+    })
+  }
 
 }
