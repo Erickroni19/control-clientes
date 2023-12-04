b192d41feff9ddf6dff3c165b24d90b3169d9330
Erick Romero <erickromeronieto018@gmail.com>
2023-12-04 16:17:38 -0500

    Termine la vista configuración

--- src/app/components/configuracion/configuracion.component.ts
+++ src/app/components/configuracion/configuracion.component.ts
@@ -11,6 +11,7 @@ import { ConfiguracionService } from 'src/app/services/configuracion.service';
 export class ConfiguracionComponent implements OnInit{
 
   permitirRegistro: boolean = false;
+  color = "#243757"
 
   constructor(private configuracionService: ConfiguracionService,
               private router: Router){}
