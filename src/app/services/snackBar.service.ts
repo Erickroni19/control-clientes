a7c106ae67d283010ae326fbd09dc1cfb8c04021
Erick Romero <erickromeronieto018@gmail.com>
2024-01-16 16:45:18 -0500

    Elimine comentarios en snackBarServices

--- src/app/services/snackBar.service.ts
+++ src/app/services/snackBar.service.ts
@@ -8,13 +8,12 @@ export class SnackBarService {
 
   constructor(private snackBar: MatSnackBar) { }
 
-  /**Snackbar: para mostrar mensajes de error ó estados de notificaciones */
   snackBarMessages(mensaje: string, accion: string, panelClass: string, position: MatSnackBarVerticalPosition){
     this.snackBar.open(mensaje, accion, {
-      duration: 3000, // Duración en milisegundos
-      verticalPosition: position, // Posición vertical
-      horizontalPosition: 'center', // Posición horizontal
-      panelClass: [panelClass], // Clase de estilo personalizada
+      duration: 3000, 
+      verticalPosition: position, 
+      horizontalPosition: 'center', 
+      panelClass: [panelClass],
     });
   }
 }
